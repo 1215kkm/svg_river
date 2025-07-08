a0676c832e64a6ee69d44bdae651d96d4093841b
1215kkm <66404083+1215kkm@users.noreply.github.com>
2025-07-08 15:52:49 +0900

    업데이트

--- script/action.js
+++ script/action.js
@@ -7,13 +7,19 @@ $(window).on('scroll', function() {
         { selector: '.ship0 svg',   start: -106, end: -1106 },
         { selector: '.ship1 svg',   start: -106, end: -1106 },
         { selector: '.ship2 svg',   start: -106, end: -1106 },
+        { selector: '.ship2-1 svg',   start: -105, end: -1105 },
+        { selector: '.ship2-2 svg',   start: -106, end: -1106 },
+        { selector: '.ship2-3 svg',   start: -113, end: -1113 },
         { selector: '.ship3 svg',   start: -113, end: -1113 },
+        { selector: '.ship3-2 svg',   start: -115, end: -1115 },
         { selector: '.ship4 svg',   start: -120, end: -1120 },
+        { selector: '.ship4-2 svg',   start: -120, end: -1120 },
         { selector: '.ship5 svg',   start: -123, end: -1123 },
+        { selector: '.ship5-2 svg',   start: -125, end: -1125 },
         { selector: '.ship6 svg',   start: -110, end: -1110 },
         { selector: '.ship7 svg',   start: -120, end: -1120 },
-        { selector: '.ship8 svg',   start: -90, end: -190 },
-        { selector: '.ship9 svg',   start: -174, end: -1174 },
+        { selector: '.ship8 svg',   start: -92, end: -1092 },
+        { selector: '.ship9 svg',   start: -172, end: -1172 },
     ];
 
     configs.forEach(function(cfg) {
